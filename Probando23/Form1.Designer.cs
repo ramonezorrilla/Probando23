6f842ff2bc06d197edff51082c6980189bda78d0
ramonezorrilla <ramoenzorrilla@gmail.com>
2026-05-02 14:17:41 -0400

    probando otra vez

--- Probando23/Form1.Designer.cs
+++ Probando23/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             button1 = new Button();
             button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
             // 
             // button1
@@ -53,11 +54,22 @@
             button2.Text = "button2";
             button2.UseVisualStyleBackColor = false;
             // 
+            // button3
+            // 
+            button3.BackColor = Color.IndianRed;
+            button3.Location = new Point(69, 73);
+            button3.Name = "button3";
+            button3.Size = new Size(350, 123);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
@@ -69,5 +81,6 @@
 
         private Button button1;
         private Button button2;
+        private Button button3;
     }
 }
